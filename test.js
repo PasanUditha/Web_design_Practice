c4a569f8efab40cfc4771ad5073af8a73e58ec39
PasanUditha <it20055080@my.sliit.lk>
2022-12-30 13:40:06 +0530

    Javascript_event

---
+++ test.js
@@ -0,0 +1,17 @@
+// variables and functions
+
+// var username ;
+// var greeting ;
+// username = "John Doe";
+// greeting = "Hi";
+
+// document.getElementById("pid").textContent = username;
+
+// function display(id, text) {
+//     document.getElementById(id).textContent = text;
+// }
+
+// events
+
+
+
