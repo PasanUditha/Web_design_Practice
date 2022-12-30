05c4ee9617217c3c016b53c3166e383997d6d655
PasanUditha <it20055080@my.sliit.lk>
2022-12-30 15:55:29 +0530

    Loop_test

--- test.js
+++ test.js
@@ -11,7 +11,26 @@
 //     document.getElementById(id).textContent = text;
 // }
 
-// events
+//  loops
+
+// FOR IN LOOP
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let text="";
+for(let x in numbers){
+    text += numbers[x];
+}
+
+//FOR OF LOOP
+
+for (let step = 0; step < 5; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log('Walking east one step');
+  }
+
+
+
 
 
 
